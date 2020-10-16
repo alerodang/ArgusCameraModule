4edf222e6d19e4527e93333495bc5a4d9ca4b0d2
Alejandro Rodríguez Angulo <alerodriguezangulo@gmail.com>
2020-10-16 12:05:36 +0200

    update

--- image-uploader-container/application/src/request_manager.py
+++ image-uploader-container/application/src/request_manager.py
@@ -1,14 +1,15 @@
 import requests
+import os
 
 class RequestManager:
 
     def post(self, url, bytes_image, path):
         body = {
-            'account':'alerodriguezangulo@gmail.com',
+            'account': os.environ.get('ACCOUNT'),
             'capture': bytes_image,
             'date': path.split('/')[2][:-4],
-            'producer': 'door camera',
-            'secret': 'somesecret'
+            'producer': os.environ.get('PRODUCER_NAME'),
+            'secret': os.environ.get('PRODUCER_SECRET')
         }
         response = requests.post(url, json = body)
         print(' [x] Request respose', response.status_code)
